5f75b844abcd376d19fcc913aa88da059590cc13
Fernando Ribas <fribas84@gmail.com>
2023-09-28 14:25:24 -0300

    cleaning spaces

--- test/Wallet.js
+++ test/Wallet.js
@@ -8,43 +8,35 @@ const ticker = ethers.encodeBytes32String("DTK");
 describe("Wallet", () => {
   const deployFixture = async () => {
     const [owner, user, user2] = await ethers.getSigners();
-
     const DummyToken = await ethers.getContractFactory("DummyToken");
     const dummyToken = await DummyToken.deploy();
-
     const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy(dummyToken.target);
-
     await dummyToken.grantMinterRoler(faucet.target);
-
     const Wallet = await ethers.getContractFactory("Wallet");
     const wallet = await Wallet.deploy();
-
     return { dummyToken, faucet, wallet, owner, user, user2 };
   };
 
   const deployAndSetTokenFixture = async () => {
     const [owner, user, user2] = await ethers.getSigners();
-
     const DummyToken = await ethers.getContractFactory("DummyToken");
     const dummyToken = await DummyToken.deploy();
-
     const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy(dummyToken.target);
-
     await dummyToken.grantMinterRoler(faucet.target);
-
     const Wallet = await ethers.getContractFactory("Wallet");
     const wallet = await Wallet.deploy();
-
     await wallet.addToken(ticker, dummyToken.target);
     return { dummyToken, faucet, wallet, owner, user, user2 };
   };
+
   it("Deploy", async () => {
     const { dummyToken, faucet, wallet, owner } = await deployFixture();
     expect(await faucet.token()).to.equal(dummyToken.target);
     expect(await wallet.owner()).to.equal(owner.address);
   });
+
   it("Owner can set Dummy Token", async () => {
     const { dummyToken, wallet } = await deployFixture();
     await wallet.addToken(ticker, dummyToken.target);
@@ -52,6 +44,7 @@ describe("Wallet", () => {
       await wallet.tokenAvailable(ticker)
     ).to.equal(true);
   });
+
   it("User cannot add Dummy Token", async () => {
     const { dummyToken, faucet, wallet, owner, user } = await deployFixture();
     expect(
@@ -61,6 +54,7 @@ describe("Wallet", () => {
       )
     ).to.be.revertedWith("Ownable: caller is not the owner");
   });
+  
   it("User can deposit Dummy Token", async () => {
     const { dummyToken, wallet, user, faucet } = await deployAndSetTokenFixture();
     await faucet.connect(user).requestTokens();
